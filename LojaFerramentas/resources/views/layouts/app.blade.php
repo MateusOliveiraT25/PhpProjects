eab3ec0922e9ae06d3b89ce56e630a616855bbfe
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-08-12 21:58:02 -0300

    12/08/24

---
+++ LojaFerramentas/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja Ferramantas</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    @include('parts.header')
+
+
+    @yield('content')
+
+    
+    @include('parts.footer')
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+</html>
---
+++ LojaFerramentas/resources/views/parts/footer.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <h1>Meu Header</h1>
+</header>
