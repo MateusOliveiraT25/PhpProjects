0f3a2794eb23c80acfc5631c8c639e7a4c0c57ac
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-08-15 19:33:35 -0300

    15/08/24

--- LojaFerramentas/app/Http/Middleware/ProdutosMiddleware.php
+++ LojaFerramentas/app/Http/Middleware/ProdutosMiddleware.php
@@ -16,7 +16,7 @@ class ProdutosMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && Auth::user()->tipo_usuario === "Administrador") {
+        if (Auth::check() && Auth::user()->tipo_usuario === "administrador") {
             return $next($request);
         }
     
