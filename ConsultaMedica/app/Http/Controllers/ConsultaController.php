e7d75cb62c76f7cff16cbd98aee36e81eb2a3bd3
MateusOliveiraT25 <oliveiramateus1222@gmail.com>
2024-08-20 12:36:42 -0300

    20/08/24

--- ConsultaMedica/app/Http/Controllers/ConsultaController.php
+++ ConsultaMedica/app/Http/Controllers/ConsultaController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Consulta;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Auth;
-
 
 class ConsultaController extends Controller
 {
@@ -15,14 +13,8 @@ class ConsultaController extends Controller
      */
     public function index()
     {
-         // Verifica se o usuário está autenticado
-         if (!Auth::check()) {
-            return redirect('/login')->withErrors('Você precisa estar logado para ver as consultas.');
-        }
-
-        $consultas = Consulta::all(); // Ajuste conforme a lógica de recuperação das consultas
-
-        return view('home', compact('consultas'));
+        $consultas = Consulta::all();
+        return view('consultas.index', compact('consultas'));
     }
 
     /**
