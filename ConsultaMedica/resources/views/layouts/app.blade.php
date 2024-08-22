d577c085138fafec08adb6c8c7d184500afb634f
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-08-21 21:27:59 -0300

    21/08/24

--- ConsultaMedica/resources/views/layouts/app.blade.php
+++ ConsultaMedica/resources/views/layouts/app.blade.php
@@ -10,6 +10,10 @@
     <!-- Incluindo o CSS personalizado -->
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
 </head>
 
 <body>
--- ConsultaMedica/resources/views/parts/footer.blade.php
+++ ConsultaMedica/resources/views/parts/footer.blade.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Consulta Médica</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
-</head>
-<body>
     <div class="wrapper">
         <div class="content container-fluid">
             <!-- Seu conteúdo aqui -->
@@ -37,10 +26,3 @@
         </footer>
     </div>
 
-
-    <!-- Scripts JS do Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</body>
-</html>
